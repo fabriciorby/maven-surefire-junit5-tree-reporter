b55197243c153bd42313fe65896e1e8209282477
fabriciorby <fabriciorby@hotmail.com>
2022-06-26 01:01:28 +0200

    add more samples

---
+++ src/test/java/NestedExampleTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Nested Sample")
+public class NestedExampleTest {
+
+    @Test
+    @DisplayName("Should pass")
+    void test() {
+    }
+
+    @Nested
+    @DisplayName("Inner Test")
+    class InnerTest {
+
+        @Test
+        @DisplayName("Inner test should pass")
+        void test() {
+        }
+
+        @Nested
+        @DisplayName("Inner Inner Test")
+        class InnerInnerTest {
+
+            @Test
+            @DisplayName("Inner Inner Test should pass")
+            void test() {
+            }
+
+            @Nested
+            @DisplayName("Inner Inner Inner Test")
+            class InnerInnerInnerTest {
+
+                @Test
+                @DisplayName("Inner Inner Inner Test should pass")
+                void test() {
+                }
+
+            }
+
+        }
+
+    }
+
+}
