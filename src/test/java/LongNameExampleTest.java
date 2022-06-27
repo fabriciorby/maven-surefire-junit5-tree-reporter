64605bea5f868a9dd96dfbf9d14ecf48c9609c30
fabriciorby <fabriciorby@hotmail.com>
2022-06-27 04:13:16 +0200

    update mockito version

--- src/test/java/LongNameExampleTest.java
+++ src/test/java/LongNameExampleTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Long Name Sample Test")
