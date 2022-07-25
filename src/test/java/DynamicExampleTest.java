bdb4f9d31b6f582f19e0c9bcf71c4c17c82907c1
fabriciorby <fabriciorby@hotmail.com>
2022-07-25 12:45:58 +0200

    add DynamicExampleTest

---
+++ src/test/java/DynamicExampleTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DynamicExampleTest {
+
+    Collection<DynamicTest> tests = Arrays.asList(
+            DynamicTest.dynamicTest("Add test",
+                    () -> assertEquals(2, Math.addExact(1, 1))),
+            DynamicTest.dynamicTest("Multiply Test",
+                    () -> assertEquals(4, Math.multiplyExact(2, 2))));
+
+    @TestFactory
+    Collection<DynamicTest> dynamicTestsWithCollection() {
+        return tests;
+    }
+
+    @TestFactory
+    @DisplayName("Calculating")
+    Collection<DynamicTest> dynamicTestsWithCollectionWithDisplayName() {
+        return tests;
+    }
+
+}
