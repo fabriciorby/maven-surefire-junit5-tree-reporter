d7bde05bf1f913bdb29ac7872f0d1119111a39d9
Andre Wachsmuth <awa@xima.de>
2023-05-07 05:32:12 +0100

    fix javadoc

--- src/main/java/org/apache/maven/plugin/surefire/loader/SurefireClassLoaderModifier.java
+++ src/main/java/org/apache/maven/plugin/surefire/loader/SurefireClassLoaderModifier.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.stream.Stream;
 
+import org.apache.maven.plugin.surefire.extensions.junit5.JUnit5StatelessTestsetInfoReporter;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -26,7 +28,7 @@ import java.util.stream.Stream;
 
 /**
  * Class created to modify the Surefire ClassLoader during Runtime.
- * Needed for {@link JUnit5StatelessTestsetInfoReporterBase#clone()} method,
+ * Needed for {@link JUnit5StatelessTestsetInfoReporter#clone()} method,
  * which is used when the unit tests are running using multiple threads.
  *
  * @author <a href="mailto:fabriciorby@hotmail.com">Fabrício Yamamoto (fabriciorby)</a>
